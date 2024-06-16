e2d4601fd0b26e1fbaa6fa23cdb0a12bff912200
thoughtsunificator <oss@thoughtsunificator.me>
2024-06-16 19:47:22 +0200

    fixup! Add comment

--- src/persistence/indexeddb-local/notes.js
+++ src/persistence/indexeddb-local/notes.js
@@ -12,7 +12,7 @@ export default properties => {
 	 */
 	listeners.push({ query: "individualNotesAdded", callback: data => {
 		const tree = geneatree.trees.list.find(tree => tree.individuals.find(individual => individual.offlineId === data.individual))
-		const individual = tree.individuals.find(individual => individual.offlineId === data.individual)
+		const individual = tree.individuals.find(individual => individual.offlineId === data.individual) // data.individual == individual.offlineId
 		const note = individual.notes.find(note => note.id === data.id)
 		note.offlineId = data.offlineId
 	}})
